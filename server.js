02d52e73c3edd23f78319b7460465c180beb2531
unknown <katapat.43@mail.kmutt.ac.th>
2023-04-10 16:34:33 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+var admin = require("firebase-admin");
+// Initialize Firebase Admin SDK with service account credentials
+var serviceAccount = require("./tactical-app-firebase-adminsdk-vfdhh-251439f642.json");
+
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://tactical-app-default-rtdb.firebaseio.com"
+});
+
+
+// Get a reference to the Realtime Database
+const database = admin.database();
+
+// Get a reference to the Firestore
+const firestore = admin.firestore();
+
+const ref = database.ref("/");
+ref.on("value", (snapshot) => {
+  
+  const data = snapshot.val();
+  console.log(data);
+  const resetData = {
+    Score:0,
+    Status: false,
+    Time: 3.00,
+  };
+  // Convert the data to an array of objects
+  // Save the data to Firestore
+    if (data.Status === true) {
+      firestore.collection("TestData").add(data);
+      ref.update(resetData)
+    }
+});
